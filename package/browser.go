22a1fefa93188e101d6be21f860c6cb3bccc7ad1
Pete Vilter <vilterp@cockroachlabs.com>
2018-02-21 22:55:23 -0500

    separate out BrowserPage

--- package/browser.go
+++ package/browser.go
@@ -2,10 +2,7 @@ package jankybrowser
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"sync"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
@@ -39,6 +36,15 @@ func NewBrowser(window *pixelgl.Window, initialURL string) *Browser {
 
 func (b *Browser) Draw(t pixel.Target) {
 	// Update & draw URL bar.
+	b.DrawChrome(t)
+
+	// Draw page.
+	b.currentPage.Draw(t)
+}
+
+// TODO: factor this out into its own DOMNode/Component which takes its own attributes
+// and emits its own events... once we have those concepts...
+func (b *Browser) DrawChrome(t pixel.Target) {
 	b.currentPage.mu.RLock()
 	str := fmt.Sprintf("%s | %s", StateNames[b.currentPage.state], b.currentPage.url)
 	if b.currentPage.state == PageStateError {
@@ -60,9 +66,6 @@ func (b *Browser) Draw(t pixel.Target) {
 	b.backButton.X = 20
 	b.backButton.Y = b.window.Bounds().H() - 15
 	b.backButton.Draw(t)
-
-	// Draw page.
-	b.currentPage.Draw(t)
 }
 
 func (b *Browser) ProcessMouseEvents(pt pixel.Vec) {
@@ -105,137 +108,4 @@ func (b *Browser) PopHistory() (string, error) {
 	url := b.history[len(b.history)-2]
 	b.history = b.history[:len(b.history)-2]
 	return url, nil
-}
-
-type PageState = int
-
-const (
-	PageStateInit PageState = iota
-	PageStateLoading
-	PageStateLoaded
-	PageStateError
-)
-
-var StateNames = map[PageState]string{
-	PageStateInit:    "INIT",
-	PageStateLoading: "LOADING",
-	PageStateLoaded:  "LOADED",
-	PageStateError:   "ERROR",
-}
-
-type BrowserPage struct {
-	mu sync.RWMutex
-
-	url       string
-	state     PageState
-	loadError error    // set when state = PageStateError
-	rootNode  dom.Node // set when state = PageStateLoaded
-}
-
-func NewBrowserPage(url string) *BrowserPage {
-	return &BrowserPage{
-		state: PageStateInit,
-		url:   url,
-	}
-}
-
-func (bp *BrowserPage) Load() {
-	go func() {
-		bp.doLoad()
-	}()
-}
-
-// doLoad is blocking. Don't call in the main UI thread.
-func (bp *BrowserPage) doLoad() {
-	bp.mu.Lock()
-	bp.state = PageStateLoading
-	bp.mu.Unlock()
-
-	response, err := http.Get(bp.url)
-
-	bp.mu.Lock()
-	defer bp.mu.Unlock()
-
-	if err != nil {
-		bp.state = PageStateError
-		bp.loadError = err
-		return
-	}
-	if response.StatusCode != 200 {
-		bp.state = PageStateError
-		// TODO: structured error
-		bp.loadError = fmt.Errorf("non-200 status code: %d", response.StatusCode)
-		return
-	}
-
-	bytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		bp.state = PageStateError
-		bp.loadError = fmt.Errorf("error reading input stream: %s", err.Error())
-		return
-	}
-
-	node, err := dom.Parse(bytes)
-	if err != nil {
-		bp.state = PageStateError
-		// TODO: structured error
-		bp.loadError = fmt.Errorf("parse error: %s", err.Error())
-		return
-	}
-
-	bp.state = PageStateLoaded
-	if node == nil {
-		node = &dom.GroupNode{}
-	}
-	bp.rootNode = node
-	log.Println("rootNode:", dom.Format(bp.rootNode))
-	bp.rootNode.Init()
-}
-
-func (bp *BrowserPage) Draw(t pixel.Target) {
-	bp.mu.RLock()
-	defer bp.mu.RUnlock()
-
-	switch bp.state {
-	case PageStateInit:
-		break
-	case PageStateLoading:
-		break
-	case PageStateLoaded:
-		bp.rootNode.Draw(t)
-	case PageStateError:
-		// TODO: render error state
-		// make a <text> element and an error DOM and use it!
-	}
-}
-
-func (bp *BrowserPage) ProcessMouseEvents(pt pixel.Vec) string {
-	hoveredNodes := bp.GetHoveredNodes(pt)
-	var navigateTo string
-	if len(hoveredNodes) > 0 {
-		//var hoveredNodeStrs []string
-		//for _, hoveredNode := range hoveredNodes {
-		//	hoveredNodeStrs = append(hoveredNodeStrs, dom.Format(hoveredNode))
-		//}
-		for _, hoveredNode := range hoveredNodes {
-			switch n := hoveredNode.(type) {
-			case *dom.GroupNode:
-				if n.Href != "" {
-					navigateTo = n.Href
-				}
-			}
-		}
-	}
-	return navigateTo
-}
-
-func (bp *BrowserPage) GetHoveredNodes(pt pixel.Vec) []dom.Node {
-	switch bp.state {
-	case PageStateLoaded:
-		bp.mu.RLock()
-		defer bp.mu.RUnlock()
-		return dom.Pick(bp.rootNode, pt)
-	default:
-		return nil
-	}
 }
---
+++ package/browser_page.go
@@ -0,0 +1,145 @@
+package jankybrowser
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/faiface/pixel"
+	"github.com/vilterp/jankybrowser/package/dom"
+)
+
+type PageState = int
+
+const (
+	PageStateInit PageState = iota
+	PageStateLoading
+	PageStateLoaded
+	PageStateError
+)
+
+var StateNames = map[PageState]string{
+	PageStateInit:    "INIT",
+	PageStateLoading: "LOADING",
+	PageStateLoaded:  "LOADED",
+	PageStateError:   "ERROR",
+}
+
+type BrowserPage struct {
+	mu sync.RWMutex
+
+	url       string
+	state     PageState
+	loadError error    // set when state = PageStateError
+	rootNode  dom.Node // set when state = PageStateLoaded
+}
+
+func NewBrowserPage(url string) *BrowserPage {
+	return &BrowserPage{
+		state: PageStateInit,
+		url:   url,
+	}
+}
+
+func (bp *BrowserPage) Load() {
+	go func() {
+		bp.doLoad()
+	}()
+}
+
+// doLoad is blocking. Don't call in the main UI thread.
+func (bp *BrowserPage) doLoad() {
+	bp.mu.Lock()
+	bp.state = PageStateLoading
+	bp.mu.Unlock()
+
+	response, err := http.Get(bp.url)
+
+	bp.mu.Lock()
+	defer bp.mu.Unlock()
+
+	if err != nil {
+		bp.state = PageStateError
+		bp.loadError = err
+		return
+	}
+	if response.StatusCode != 200 {
+		bp.state = PageStateError
+		// TODO: structured error
+		bp.loadError = fmt.Errorf("non-200 status code: %d", response.StatusCode)
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		bp.state = PageStateError
+		bp.loadError = fmt.Errorf("error reading input stream: %s", err.Error())
+		return
+	}
+
+	node, err := dom.Parse(bytes)
+	if err != nil {
+		bp.state = PageStateError
+		// TODO: structured error
+		bp.loadError = fmt.Errorf("parse error: %s", err.Error())
+		return
+	}
+
+	bp.state = PageStateLoaded
+	if node == nil {
+		node = &dom.GroupNode{}
+	}
+	bp.rootNode = node
+	log.Println("parsed DOM tree:", dom.Format(bp.rootNode))
+	bp.rootNode.Init()
+}
+
+func (bp *BrowserPage) Draw(t pixel.Target) {
+	bp.mu.RLock()
+	defer bp.mu.RUnlock()
+
+	switch bp.state {
+	case PageStateInit:
+		break
+	case PageStateLoading:
+		break
+	case PageStateLoaded:
+		bp.rootNode.Draw(t)
+	case PageStateError:
+		// TODO: render error state
+		// make a <text> element and an error DOM and use it!
+	}
+}
+
+func (bp *BrowserPage) ProcessMouseEvents(pt pixel.Vec) string {
+	hoveredNodes := bp.GetHoveredNodes(pt)
+	var navigateTo string
+	if len(hoveredNodes) > 0 {
+		//var hoveredNodeStrs []string
+		//for _, hoveredNode := range hoveredNodes {
+		//	hoveredNodeStrs = append(hoveredNodeStrs, dom.Format(hoveredNode))
+		//}
+		for _, hoveredNode := range hoveredNodes {
+			switch n := hoveredNode.(type) {
+			case *dom.GroupNode:
+				if n.Href != "" {
+					navigateTo = n.Href
+				}
+			}
+		}
+	}
+	return navigateTo
+}
+
+func (bp *BrowserPage) GetHoveredNodes(pt pixel.Vec) []dom.Node {
+	switch bp.state {
+	case PageStateLoaded:
+		bp.mu.RLock()
+		defer bp.mu.RUnlock()
+		return dom.Pick(bp.rootNode, pt)
+	default:
+		return nil
+	}
+}
